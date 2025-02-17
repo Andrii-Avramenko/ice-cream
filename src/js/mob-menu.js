58516d3839bf4be9af814285c8f86ef39be3edd0
Andrii Avramenko <andreitvmm228@gmail.com>
2025-02-17 21:05:10 +0000

    Заміна шляхів, респонсив для логотипа та новий код для мобільного меню

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -4,15 +4,17 @@
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    console.log('isMenuOpen', isMenuOpen)
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    openMenuBtn.classList.toggle('visually-hidden');
     mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
+
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -20,11 +22,15 @@
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    console.log('width')
+    if (!e.matches) {
+      openMenuBtn.classList.toggle('visually-hidden');
+      return;
+    }
+    else {
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    }
   });
-  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  openMenuBtn.classList.toggle('visually-hidden');
 })();
